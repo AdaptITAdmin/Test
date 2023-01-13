adc08a84b0cb4a41053d775fc42b7eb22bf429db
AdaptITSPardy <122602615+AdaptITSPardy@users.noreply.github.com>
2023-01-13 14:46:32 +0000

    Update writetofile.ps1

--- writetofile.ps1
+++ writetofile.ps1
@@ -1,8 +1,8 @@
 # Note that you will need to specify NoNewLine for the first entry as well
-'BlueBerry;' | Out-File -FilePath C:\Desktop\fruits.txt -NoNewline
+'BlueBerry;' | Out-File -FilePath C:\fruits.txt -NoNewline
 
 # Appending more berries
-'StrawBerry;' | Out-File -FilePath C:\Desktop\fruits.txt -Append -NoNewline
-'BlackBerry;' | Out-File -FilePath C:\Desktop\fruits.txt -Append -NoNewline
-'RaspBerry;' | Out-File -FilePath C:\Desktop\fruits.txt -Append -NoNewline
-'CranBerry;' | Out-File -FilePath C:\Desktop\fruits.txt -Append -NoNewline
+'StrawBerry;' | Out-File -FilePath C:\fruits.txt -Append -NoNewline
+'BlackBerry;' | Out-File -FilePath C:\fruits.txt -Append -NoNewline
+'RaspBerry;' | Out-File -FilePath C:\fruits.txt -Append -NoNewline
+'CranBerry;' | Out-File -FilePath C:\fruits.txt -Append -NoNewline
