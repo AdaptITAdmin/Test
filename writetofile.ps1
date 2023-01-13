e74c3ca4a89b050b177170f63d4c616f5cca0355
AdaptITSPardy <122602615+AdaptITSPardy@users.noreply.github.com>
2023-01-13 15:06:34 +0000

    Update writetofile.ps1

--- writetofile.ps1
+++ writetofile.ps1
@@ -1,10 +1,10 @@
 # Note that you will need to specify NoNewLine for the first entry as well
-Set-Location -Path "/test/copytest/fruits.txt"
-get-location
-# 'BlueBerry;' | Out-File -FilePath $path -NoNewline
+# Set-Location -Path "/test/copytest/fruits.txt"
+# get-location
+'BlueBerry;' | Out-File -FilePath "fruits.txt" -NoNewline
 
-# # Appending more berries
-# 'StrawBerry;' | Out-File -FilePath $path -Append -NoNewline
-# 'BlackBerry;' | Out-File -FilePath $path -Append -NoNewline
-# 'RaspBerry;' | Out-File -FilePath  $path -Append -NoNewline
-# 'CranBerry;' | Out-File -FilePath  $path -Append -NoNewline
+# Appending more berries
+'StrawBerry;' | Out-File -FilePath "fruits.txt" -Append -NoNewline
+'BlackBerry;' | Out-File -FilePath "fruits.txt" -Append -NoNewline
+'RaspBerry;' | Out-File -FilePath  "fruits.txt" -Append -NoNewline
+'CranBerry;' | Out-File -FilePath  "fruits.txt" -Append -NoNewline
